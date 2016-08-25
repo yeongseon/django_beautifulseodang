d6a3e689b500b3b51eed9ce89c29e2698f70dfd3
yeongseon choe <yeongseon.choe@gmail.com>
2016-08-25 15:04:36 +0900

    Add home app

---
+++ home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.views.generic import View
+
+from django.views.generic import TemplateView
+from django.contrib import messages
+
+# Create your views here.
+
+class MyView(View):
+    def get(self, request):
+        return HttpResponse('result')
+
+class HomePageView(TemplateView):
+    template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        messages.info(self.request, 'hello http://example.com')
+        return context
